b4d1e3779ebf54011100aa88d8b52e7c56a9cb5e
Isaac Vicente <contatoisaacvicente@gmail.com>
2023-01-15 21:08:28 -0300

    Update tmux, vim; little changes in nvim plugins and zsh

--- private_dot_config/nvim/lua/custom/plugins.lua
+++ private_dot_config/nvim/lua/custom/plugins.lua
@@ -2,6 +2,8 @@ return function(use)
   use({
     "folke/which-key.nvim",
       config = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 200
         require("which-key").setup({})
       end
   })
