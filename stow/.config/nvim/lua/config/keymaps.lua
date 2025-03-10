6808916cfd8377e54e9db2b3feeccd8eb7da253e
Isaac Vicente <isaacvicentsocial@gmail.com>
2025-03-10 14:02:44 -0300

    feat(neovim): floating terminal with `Snacks.terminal`

--- stow/.config/nvim/lua/config/keymaps.lua
+++ stow/.config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,21 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "-", "<cmd>Oil<CR>")
+local map = vim.keymap.set
+
+-- oil.nvim
+map("n", "-", "<cmd>Oil<CR>")
+
+-- floating terminal
+map("n", "<leader>fT", function()
+  Snacks.terminal()
+end, { desc = "Terminal (cwd)" })
+map("n", "<leader>ft", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+map("n", "<c-/>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+map("n", "<c-_>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "which_key_ignore" })
---
+++ stow/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      win = {
+        style = "terminal",
+        position = "float",
+        backdrop = 60,
+        height = 0.8,
+        width = 0.8,
+        zindex = 50,
+        border = "rounded",
+      },
+    },
+  },
+}
